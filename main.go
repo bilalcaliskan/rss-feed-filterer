ef4265992a0b43bbe260710f0828bafc9560e118
bilalcaliskan <bilalcaliskan@protonmail.com>
2023-08-04 22:13:57 +0300

    feat: add semaphore

--- main.go
+++ main.go
@@ -9,16 +9,10 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
-type Config struct {
-	Repositories []Repository `yaml:"repositories"`
-}
+const maxRetries = 3
 
-type Repository struct {
-	Name                 string `yaml:"name"`
-	Description          string `yaml:"description,omitempty"`
-	RSSURL               string `yaml:"rssURL"`
-	CheckIntervalMinutes int    `yaml:"checkIntervalMinutes"`
-}
+// create a channel to act as a semaphore
+var sem = make(chan struct{}, 5) // allow up to 5 concurrent access
 
 func readConfig() Config {
 	var config Config
@@ -34,21 +28,30 @@ func readConfig() Config {
 }
 
 func checkGithubReleases(repo Repository, interval time.Duration) {
+	sem <- struct{}{}        // acquire the semaphore
+	defer func() { <-sem }() // release the semaphore when done
+
 	fp := gofeed.NewParser()
 
 	checkFeed := func() {
-		feed, err := fp.ParseURL(repo.RSSURL)
-		if err != nil {
-			log.Printf("Error fetching feed: %v", err)
-			return
-		}
+		for retries := 0; retries < maxRetries; retries++ {
+			feed, err := fp.ParseURL(repo.RSSURL)
+			if err != nil {
+				log.Printf("Error fetching feed: %v, retrying...", err)
+				time.Sleep(time.Second * time.Duration(2<<retries))
+				continue
+			}
 
-		for _, item := range feed.Items {
-			log.Printf("Title: %v\nLink: %v\nPublished: %v\n", item.Title, item.Link, item.Published)
+			for _, item := range feed.Items {
+				log.Printf("Title: %v\nLink: %v\nPublished: %v\n", item.Title, item.Link, item.Published)
+			}
+
+			break
 		}
 	}
 
-	checkFeed() // Run once immediately
+	// Run once immediately since ticker will wait for first attempt
+	checkFeed()
 
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
---
+++ types.go
@@ -0,0 +1,12 @@
+package main
+
+type Config struct {
+	Repositories []Repository `yaml:"repositories"`
+}
+
+type Repository struct {
+	Name                 string `yaml:"name"`
+	Description          string `yaml:"description,omitempty"`
+	RSSURL               string `yaml:"rssURL"`
+	CheckIntervalMinutes int    `yaml:"checkIntervalMinutes"`
+}
