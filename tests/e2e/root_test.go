c2e98b570e1615861fd1cfc8addf5fce4c5397f7
bilalcaliskan <bilalcaliskan@protonmail.com>
2023-09-04 20:47:08 +0300

    test: continued

--- tests/e2e/root_test.go
+++ tests/e2e/root_test.go
@@ -4,32 +4,47 @@ package e2e
 
 import (
 	"context"
-	"fmt"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bilalcaliskan/rss-feed-filterer/cmd/root"
-	"github.com/bilalcaliskan/rss-feed-filterer/cmd/root/options"
+	internalaws "github.com/bilalcaliskan/rss-feed-filterer/internal/storage/aws"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestExecuteRootCmd(t *testing.T) {
-	rootOpts := options.GetRootOptions()
+	//rootOpts := options.GetRootOptions()
 	ctx := context.Background()
 	root.RootCmd.SetContext(ctx)
 
 	cases := []struct {
-		caseName   string
-		args       []string
-		shouldPass bool
+		caseName      string
+		args          []string
+		shouldPass    bool
+		getObjectFunc func(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
 	}{
 		{
 			"aldsfkaslfkj",
-			[]string{},
+			[]string{"--config-file=../../resources/sample_config.yaml"},
 			true,
+			func(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error) {
+				return &s3.GetObjectOutput{}, nil
+			},
 		},
 	}
 
 	for _, tc := range cases {
 		t.Logf("starting case %s\n", tc.caseName)
 
-		fmt.Println(rootOpts)
+		mockS3 := new(internalaws.MockS3Client)
+		mockS3.GetObjectAPI = tc.getObjectFunc
+
+		root.RootCmd.SetContext(context.WithValue(root.RootCmd.Context(), S3ClientKey{}, mockS3))
+		root.RootCmd.SetArgs(tc.args)
+		err := root.RootCmd.Execute()
+		if tc.shouldPass {
+			assert.Nil(t, err)
+		} else {
+			assert.NotNil(t, err)
+		}
 	}
 }
